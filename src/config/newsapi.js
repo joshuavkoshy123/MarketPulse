c0e8cdb658b51497522fc0034bcec025c8d1d060
Joshua Koshy <joshuavkoshy@gmail.com>
2025-08-21 22:19:58 -0500

    fix .env

--- src/config/newsapi.js
+++ src/config/newsapi.js
@@ -1,4 +1,4 @@
 export const newsapi = {
     api_token: process.env.REACT_APP_NEWSAPI_KEY,
-    base_url: process.env.REACT_APP_NEWSAPI_KEY
+    base_url: process.env.REACT_APP_NEWSAPI_BASE_URL
 };
